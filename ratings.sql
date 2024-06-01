42fc5cd9f1ecd0ac323fddf1a007561d86e41bc8
Sarp Ozcetin <sarpozcetin@gmail.com>
2024-06-01 12:32:06 -0700

    Added comments

--- ratings.sql
+++ ratings.sql
@@ -2,10 +2,24 @@ DROP DATABASE IF EXISTS `movie`;
 CREATE DATABASE `movie`;
 USE `movie`;
 
+/*
+Shows all ratings after January 1st, 2005, only displays 50 rows
+*/
 SELECT * FROM `ratings` WHERE `timestamp` > 1104537600 LIMIT 50;
 
+/*
+Shows all ratings after January 1st, 2005 and only ratings from user 270896
+*/
 SELECT * FROM `ratings` WHERE `timestamp` > 1104537600 AND `userid` = 270896;
 
+/*
+Shows all ratings after January 1st, 2005 and only ratings from user 270896 with a rating higher than 3.0
+orders the data by ascending rating
+*/
 SELECT * FROM `ratings` WHERE `timestamp` > 1104537600 AND `userid` = 270896 AND `rating` > 3.0 ORDER BY `rating` ASC;
 
+/*
+Selects only the movieid and rating columns and displays only ratings after January 1st, 2005
+and 4.0 or higher
+*/
 SELECT `movieid`, `rating` FROM `ratings` WHERE `timestamp` > 1104537600 AND `rating` >= 4.0;
