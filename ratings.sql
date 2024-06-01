44309d51f97337aa4580f693ece9cbd5e646b07f
Sarp Ozcetin <sarpozcetin@gmail.com>
2024-06-01 13:17:30 -0700

    Added LIMIT to each query and a join clause to the last query

--- ratings.sql
+++ ratings.sql
@@ -5,17 +5,20 @@ SELECT * FROM `ratings` WHERE `timestamp` > 1104537600 LIMIT 50;
 
 /*
 Shows all ratings after January 1st, 2005 and only ratings from user 270896
+only displays 50 rows
 */
-SELECT * FROM `ratings` WHERE `timestamp` > 1104537600 AND `userid` = 270896;
+SELECT * FROM `ratings` WHERE `timestamp` > 1104537600 AND `userid` = 270896 LIMIT 50;
 
 /*
 Shows all ratings after January 1st, 2005 and only ratings from user 270896 with a rating higher than 3.0
-orders the data by ascending rating
+orders the data by ascending rating only displays 50 rows
 */
-SELECT * FROM `ratings` WHERE `timestamp` > 1104537600 AND `userid` = 270896 AND `rating` > 3.0 ORDER BY `rating` ASC;
+SELECT * FROM `ratings` WHERE `timestamp` > 1104537600 AND `userid` = 270896 AND `rating` > 3.0 ORDER BY `rating` ASC LIMIT 50;
 
 /*
-Selects only the movieid and rating columns and displays only ratings after January 1st, 2005
-and 4.0 or higher
+Displays the movie title and rating along with the userid associated with that rating
+the movie title is found by joining the movie id from the ratings table to the movies table
 */
-SELECT `movieid`, `rating` FROM `ratings` WHERE `timestamp` > 1104537600 AND `rating` >= 4.0;
+SELECT ratings.userid, ratings.rating, movies.title FROM ratings
+INNER JOIN movies ON ratings.movieID = movies.id
+WHERE timestamp > 1104537600 AND rating >= 4.0 LIMIT 50;
