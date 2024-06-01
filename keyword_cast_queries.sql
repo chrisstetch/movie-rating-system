638c43cba953843a8f9062f13e63885ede4ad0c9
Chris Stetchman <chrisstetch@gmail.com>
2024-06-01 12:36:39 -0700

    Added queries for movie, keyword, and cast data

---
+++ keyword_cast_queries.sql
@@ -0,0 +1,2 @@
+SELECT `keywords` FROM `keywords` WHERE id=862;
+SELECT `cast` FROM `credits` WHERE id=8844;
---
+++ movie_queries.sql
@@ -0,0 +1,8 @@
+/*
+Shows the most profitable movies in the data set 
+*/
+SELECT `title`, `revenue`, `budget`, (`revenue` - `budget`) AS `profit` FROM `Movies` ORDER BY `profit` DESC LIMIT 100;
+/*
+Shows all Harry Potter movies in the dataset
+*/
+SELECT `title`, `revenue` FROM `movies` WHERE `title` LIKE '%Harry Potter%' ORDER BY `id` LIMIT 100;
