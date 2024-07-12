00319f06ad1ad996d81d3e1b165e6f9c30a2b493
Chris Stetchman <chrisstetch@gmail.com>
2024-07-12 15:37:52 -0700

    Added isolation levels, procedures, rollbacks/error handling

--- transactions.sql
+++ transactions.sql
@@ -1,19 +1,60 @@
 /* Promotes user to admin using userid */
-START TRANSACTION;
+DROP PROCEDURE IF EXISTS PromoteUser;
+DELIMITER //
+CREATE PROCEDURE PromoteUser(IN id INT)
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+    START TRANSACTION;
     UPDATE `users`
-    SET `role` = 'admin'
-    WHERE `userid` =  2;
-COMMIT;
+    SET `role` = 'user'
+    WHERE `userid` = id;
+    IF (ROW_COUNT() = 1) THEN
+        COMMIT;
+    ELSE
+        ROLLBACK;
+    END IF;
+END //
+DELIMITER ;
+
+/*  Call procedure (userid) */
+CALL PromoteUser(1);
 
 /* Delete rating from movie */
-START TRANSACTION;
+DROP PROCEDURE IF EXISTS DeleteRating;
+DELIMITER //
+CREATE PROCEDURE DeleteRating(IN userId INT, IN movieId INT)
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+    START TRANSACTION;
     DELETE FROM `ratings`
-    WHERE `userId` = 1 AND `movieId` = 110;
-COMMIT;
+    WHERE `userId` = userId AND `movieId` = movieId;
+    IF (ROW_COUNT() = 1) THEN
+        COMMIT;
+    ELSE
+        ROLLBACK;
+    END IF;
+END //
+DELIMITER ;
+
+/* Call procedure (userid, movieid) */
+CALL DeleteRating(1, 110);
 
 /* Add rating to movie */
-START TRANSACTION;
+DROP PROCEDURE IF EXISTS AddRating;
+DELIMITER //
+CREATE PROCEDURE AddRating(IN userId INT, IN movieId INT, IN rating FLOAT, IN timestamp INT)
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+    START TRANSACTION;
     INSERT INTO `ratings`(userId, movieId, rating, timestamp)
-    VALUES (1, 110, 1.0, 1425941529);
-COMMIT;
+    VALUES (userId, movieId, rating, timestamp);
+    IF (ROW_COUNT() = 1) THEN
+        COMMIT;
+    ELSE
+        ROLLBACK;
+    END IF;
+END //
+DELIMITER ;
 
+/* Call procedure (userid, movieid, rating, timestamp) */
+CALL AddRating(1, 110, 1.0, 1425941529);
