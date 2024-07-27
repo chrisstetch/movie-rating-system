f42a352aa800347087504e8f5e7965583950db0a
Chris Stetchman <chrisstetch@gmail.com>
2024-07-27 14:03:02 -0700

    Added queries that utilize NULL values

---
+++ nulls.sql
@@ -0,0 +1,23 @@
+/* Gets movies without any ratings */
+SELECT `movies`.`title`
+FROM `movies`
+LEFT JOIN `ratings` ON `ratings`.`movieID` = `movies`.`id`
+WHERE `ratings`.`rating` IS NULL;
+
+/* Lists movies that do not have an assigned genre */
+SELECT `movies`.`title`
+FROM `movies`
+LEFT JOIN `genres` ON `movies`.`id` = `genres`.`id`
+WHERE `genres`.`genre` IS NULL;
+
+/* Gets userid of users who have not rated a specific genre of movie */
+SELECT DISTINCT `ratings`.`userid`
+FROM `ratings`
+WHERE `ratings`.`userid` NOT IN (
+    SELECT `ratings`.`userid`
+    FROM `ratings`
+    JOIN `movies` ON `ratings`.`movieID` = `movies`.`id`
+     JOIN `genres` ON `movies`.`id` = `genres`.`id`
+    WHERE `genres`.`genre` = 'Comedy'
+);
+
